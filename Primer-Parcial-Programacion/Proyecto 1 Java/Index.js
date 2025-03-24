cb2c52747b625fc16afea6b47ce0dacfec7de0d8
IrisMartinez45 <marilymartinez211@gmail.com>
2025-03-24 16:16:53 -0600

    trabajos programacion

---
+++ Primer-Parcial-Programacion/Proyecto 1 Java/Index.js
@@ -0,0 +1,10 @@
+//alert ("Hola Mundo");
+//const nombre=prompt('Ingrese el nombre');
+//document.querySelector('.contenido').innerHTML=`${nombre} Está trabajando con Java`
+console.log("Hola mundo desde consola");
+let numero1=prompt("Ingrese un valor numerico");
+let numero2=prompt("Ingrese numero 2");
+console.log(numero1);
+console.log(numero2);
+//suma de valores desde la consola
+console.log("La suma de los valores es: " + (parseInt(numero1)+parseInt(numero1)));
